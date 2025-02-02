1a93e91fec37f6669e96dc8c410c60d910e011d9
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2025-02-02 19:51:24 +0600

    fetch images & data from asset.js

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
+import { assets, projectsData } from '../assets/assets'
 
 const Projects = () => {
   return (
     <div className='container mx-auto py-4 pt-20 px-6 md:px-20 lg:px-32 my-20 w-full overflow-hidden' id='Projects'>
-      <h1>Projects <span>Completed</span> </h1>
-      <p>Crafting Spaces, Building Legacies-Explorre Our Portfolio</p>
+      <h1 className='text-2xl sm:text-4xl font-bold mb-2 text-center'>Projects <span className='underline underline-offset-4 decoration-1 under font-light'>Completed</span> </h1>
+      <p className='text-center text-gray-500 mb-8 max-w-80 mx-auto'>Crafting Spaces, Building Legacies-Explorre Our Portfolio</p>
+
+    {/* slider buttons */}
+
+    <div className='flex justify-end items-center mb-8'>
+      <button className="p-3 bg-gray-200 rounded mr-2 " aria-braillelabel='Previous Project'>
+        <img src={assets.left_arrow} alt="Previous" />
+      </button>
+      <button className="p-3 bg-gray-200 rounded mr-2 " aria-braillelabel='Next Project'>
+        <img src={assets.right_arrow} alt="Next" />
+      </button>
+    </div>
+
+    {/* project slider container  */}
+
+    <div>
+      <div>
+        {projectsData.map((project, index) => (
+          <div key={index}>
+            <img src={project.image} alt={project.title} />
+          </div>
+        ))}
+      </div>
+    </div>
+
     </div>
   )
 }
