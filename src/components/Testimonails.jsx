552598c7e1e92f623a157aee7fc2a5f493024384
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2025-02-09 19:45:24 +0600

    start working with testimonials section

---
+++ src/components/Testimonails.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Testimonails = () => {
+  return (
+    <div>
+      <h1>Customer <span>Testimonials</span> </h1>
+      <p>Real Stories from Those Who Found Home with US</p>
+    </div>
+  )
+}
+
+export default Testimonails
