55c4d07179fb062085439cb79550a29f33f9908d
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2025-02-25 00:52:07 +0600

    added Footer section

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Testimonails from "./components/Testimonails"
 import Contact from "./components/Contact"
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Footer from "./components/Footer"
 
 const App = ()  =>{
   return (
@@ -17,6 +18,7 @@ const App = ()  =>{
       <Projects />
       <Testimonails/>
       <Contact />
+      <Footer />
     </div>
   )
 }
