b5b9db9c4697ec29230e938f13f55b548c519075
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2025-02-24 19:36:15 +0600

    added web3forms functionality for realtime interaction

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,12 +1,42 @@
 import React from 'react'
 
 const Contact = () => {
+
+  const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "ded2109e-66a0-4046-8076-8bf554fcb849");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("");
+      alert("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      alert(data.message);
+      //setResult(data.message);
+      setResult("");
+    }
+  };
+
+
   return (
     <div className='text-center p-6 py-20 lg:px-32 w-full overflow-hidden' id='Contact'>
       <h1 className='text-2xl sm:text-4xl font-bold mb-2 text-center'>Contact <span className='underline underline-offset-4 decoration-1 under font-light'>With Us</span> </h1>
       <p className='text-center text-gray-500 mb-12 max-w-80 mx-auto'>Ready to Make a Move? Let's Build Your Future Togather</p>
 
-    <form className='max-w-2xl mx-auto text-gray-600 pt-8'>
+    <form onSubmit={onSubmit} className='max-w-2xl mx-auto text-gray-600 pt-8'>
       <div className='flex flex-wrap'>
         <div className='w-full md:w-1/2 text-left'>
           Your Name
@@ -18,7 +48,14 @@ const Contact = () => {
           <input className='w-full border border-gray-300 rounded py-3 px-4 mt-2' type="email" name='Email' placeholder='Your Email' required />
         </div>
       </div>
-      
+
+      <div>
+        Message 
+        <textarea className='w-full border border-gray-300 rounded py-3 px-4 mt-2 h-48 resize-none' name="Message" placeholder='Message' required></textarea>
+      </div>
+
+      <button className='bg-blue-600 text-white py-2 px-12 mb-10 rounded'> {result ? result : "Send Message"} </button>
+
     </form>
     
 
