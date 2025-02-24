16eed668dcde605e428de35316bd2096eca30131
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2025-02-25 01:24:17 +0600

    added animation in Projects section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { assets, projectsData } from "../assets/assets";
+import { motion } from "framer-motion";
 
 const Projects = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -30,7 +31,11 @@ const Projects = () => {
   };
 
   return (
-    <div
+    <motion.div
+      initial={{ opacity: 0, x: -300 }}
+      transition={{ duration: 1.7 }}
+      whileInView={{ opacity: 1, x: 0 }}
+      viewport={{ once: true }}
       className="container mx-auto py-4 pt-20 px-6 md:px-20 lg:px-32 my-20 w-full overflow-hidden"
       id="Projects"
     >
@@ -85,7 +90,8 @@ const Projects = () => {
                     {project.title}
                   </h2>
                   <p className="text-gray-500 text-sm">
-                    {project.price} <span className="px-1"> | </span> {project.location}
+                    {project.price} <span className="px-1"> | </span>{" "}
+                    {project.location}
                   </p>
                 </div>
               </div>
@@ -93,7 +99,7 @@ const Projects = () => {
           ))}
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
